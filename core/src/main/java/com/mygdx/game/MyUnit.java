d15c37e4800e43b3b75f4ef7c0bf80e8a8ec2b87
aymanish <80852613+aymanish@users.noreply.github.com>
2022-03-06 20:47:21 +0000

    Add files via upload

---
+++ core/src/main/java/com/mygdx/game/MyUnit.java
@@ -0,0 +1,7 @@
+package com.mygdx.game;
+
+public class MyUnit {
+    public String concatenate(String one, String two) {
+        return one + two;
+    }
+}
---
+++ core/src/test/java/MyUnitTest.java
@@ -0,0 +1,16 @@
+import com.mygdx.game.MyUnit;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testConcatenate() {
+        MyUnit myUnit = new MyUnit();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+}
